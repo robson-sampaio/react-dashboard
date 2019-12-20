f9f54882edb58d854329407b5fdfabea03d70ae6
robson-sampaio <robson.sampaio@dellead.com>
2019-12-20 04:54:04 -0300

    Resolving alignment problems. Badge in faturamento

--- src/components/resumo/resumo.js
+++ src/components/resumo/resumo.js
@@ -23,52 +23,107 @@ class Resumo extends Component {
     render() { 
         return ( 
             <div>
-                <h2 className="resumo-title">RESUMO</h2>
-                <div className="col">
-                    <h3>Faturamento</h3>
-                    <div className="row">
-                        <div className="col">
-                            <div class="card">
-                                <div class="card-header">
-                                    30 dias anteriores
-                                </div>
-                                <div class="card-body">
-                                    <p>{this.state.consultas.consultas_30dias_anteriores}</p>
+                <h2 className="mt-2 text-left mx-4">RESUMO</h2>
+                <div className="row mx-2">
+                    <div className="col">
+                        <div className="row mx-0">
+                            <h3>Consultas</h3>
+                        </div>
+                        <div className="row">
+                            <div className="col">
+                                <div className="card mt-2 text-center">
+                                    <div className="card-header"> 30 dias anteriores</div>
+                                    <div className="card-body">{this.state.consultas.consultas_30dias_anteriores}</div>
                                 </div>
                             </div>
+                            <div className="col">
+                                <div className="card mt-2 text-center">
+                                    <div className="card-header">Próximos 30 dias</div>
+                                    <div className="card-body">{this.state.consultas.consultas_30dias_posteriores}</div>
+                                </div>                            
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="row mx-0">
+                            <h3>Faturamento</h3>
+                        </div>
+                        <div className="row">
+                            <div className="col">
+                                <div className="card mt-2 text-center">
+                                    <div className="card-header"> 30 dias anteriores</div>
+                                    <div className="card-body">
+                                        {this.state.faturamento.anterior.valor.toLocaleString("pt-BR", {style: "currency", currency: "BRL"})}
+                                        <span className="badge badge-success ml-1">
+                                            {this.state.faturamento.previsao.comparativo} %
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col">
+                                <div className="card mt-2 text-center">
+                                    <div className="card-header">Próximos 30 dias</div>
+                                    <div className="card-body">
+                                        {this.state.faturamento.previsao.valor.toLocaleString("pt-BR", {style: "currency", currency: "BRL"})}
+                                        <span className="badge badge-danger ml-1">
+                                            {this.state.faturamento.previsao.comparativo} %
+                                        </span>
+                                    </div>
+                                </div>                            
+                            </div>
+                        </div>
+                    </div>
+                </div>                
+                
+                {/* <h2 className="mt-2 text-left">RESUMO</h2>
+                <div className="row">
+                    <div className="col">
+                        <h3>Consultas</h3>
+                        <div className="row">
+                            <div className="col">                            
+                                <div class="card mt-2 text-center">
+                                    <div class="card-header">
+                                        30 dias anteriores
+                                    </div>
+                                    <div class="card-body">
+                                        <p>{this.state.consultas.consultas_30dias_anteriores}</p>
+                                    </div>
+                                </div>
+                            </div> 
+                            <div className="col">
+                                <div class="card">
+                                    <div class="card-header">
+                                        30 dias anteriores
+                                    </div>
+                                    <div class="card-body">
+                                        <p>{this.state.consultas.consultas_30dias_anteriores}</p>
+                                    </div>
+                                </div>
+                            </div>  
+                            <div className="col">
+                                <h3>Faturamento</h3>
+                                <div class="card">
+                                    <div class="card-header">
+                                        30 dias anteriores
+                                    </div>
+                                    <div class="card-body">
+                                        <p>{this.state.consultas.consultas_30dias_anteriores}</p>
+                                    </div>
+                                </div>
+                            </div> 
+                            <div className="col">
+                                <div class="card">
+                                    <div class="card-header">
+                                        30 dias anteriores
+                                    </div>
+                                    <div class="card-body">
+                                        <p>{this.state.consultas.consultas_30dias_anteriores}</p>
+                                    </div>
+                                </div>
+                            </div> 
                         </div> 
-                        <div className="col">
-                            <div class="card">
-                                <div class="card-header">
-                                    30 dias anteriores
-                                </div>
-                                <div class="card-body">
-                                    <p>{this.state.consultas.consultas_30dias_anteriores}</p>
-                                </div>
-                            </div>
-                        </div>  
-                        <div className="col">
-                            <div class="card">
-                                <div class="card-header">
-                                    30 dias anteriores
-                                </div>
-                                <div class="card-body">
-                                    <p>{this.state.consultas.consultas_30dias_anteriores}</p>
-                                </div>
-                            </div>
-                        </div> 
-                        <div className="col">
-                            <div class="card">
-                                <div class="card-header">
-                                    30 dias anteriores
-                                </div>
-                                <div class="card-body">
-                                    <p>{this.state.consultas.consultas_30dias_anteriores}</p>
-                                </div>
-                            </div>
-                        </div> 
-                    </div> 
-                </div>                              
+                    </div>                              
+                </div> */}
             </div>
          )
     }
